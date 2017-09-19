e0afb67d736ad1f918d7e761f34ff7d0625a9de1
daymisono <flower_op4@naver.com>
2017-09-19 10:44:06 +0900

    커밋

--- MariahDBExample001/MariahDBExample001/Program.cs
+++ MariahDBExample001/MariahDBExample001/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace MariahDBExample001
 {
@@ -10,7 +11,26 @@ namespace MariahDBExample001
     {
         static void Main(string[] args)
         {
+            string MyConString = "SERVER=localhost; DATABASE=destinyecm; UID=ghostone; PASSWORD=ghostone;";
+            MySqlConnection connecntion = new MySqlConnection(MyConString);
+            MySqlCommand command = connecntion.CreateCommand();
+            MySqlDataReader Reader;
+            command.CommandText = "select * from XProcessInfo";
+            connecntion.Open();
+            Reader = command.ExecuteReader();
 
+            StringBuilder sb = new StringBuilder();
+
+            while (Reader.Read())
+            {
+                string thisrow = "";
+                for(int i = 0; i < Reader.FieldCount; i++)
+                    thisrow += Reader.GetValue(i).ToString() + ",";
+                sb.AppendLine(thisrow);
+            }
+            connecntion.Close();
+
+            Console.WriteLine(sb.ToString());
         }
     }
 }
